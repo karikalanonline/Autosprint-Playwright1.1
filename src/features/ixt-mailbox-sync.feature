8d6d5c3c46ef83836ba90d223271f229cd61ee20
Venkatachalam Karikalan <karvenkatachalam@deloitte.com>
2025-11-06 15:13:30 +0530

    Updated code

--- src/features/ixt-mailbox-sync.feature
+++ src/features/ixt-mailbox-sync.feature
@@ -1,3 +1,6 @@
 Feature: Ixt Mailbox Sync
     Background:
         Given I am logged into Salesforce
+        And Navigate to the IXT WebForm Page
+
+    Scenario: Create a case via webform
--- src/step_definitions/common-steps.ts
+++ src/step_definitions/common-steps.ts
@@ -1,7 +1,12 @@
 import { Given } from "@cucumber/cucumber";
 import { CustomWorld } from "../support/custom-world";
 import { loginToSalesforce } from "../flows/auth.flows";
+import { goToIxtWebForm } from "src/flows/ixt.flow";
 
 Given("I am logged into Salesforce", async function (this: CustomWorld) {
   await loginToSalesforce(this);
+});
+
+Given("Navigate to the IXT WebForm Page", async function (this: CustomWorld) {
+  await goToIxtWebForm(this);
 });
